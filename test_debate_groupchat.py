c463bd0a4b7e4971df7d8e57114881a0c6b3b159
Mark Sze <66362098+marklysze@users.noreply.github.com>
2024-06-25 08:24:31 +1000

    Update test_debate_groupchat.py

--- test_debate_groupchat.py
+++ test_debate_groupchat.py
@@ -39,12 +39,7 @@ class debate:
         os.environ["AUTOGEN_USE_DOCKER"] = "False"
 
     def load_team(self):
-        #from urllib.parse import urlparse
-        #from autogen.agentchat.contrib.agent_builder import AgentBuilder
-
         file_name = self.saved_team
-        # new_builder = AgentBuilder(config_file_or_env=self.config_file_or_env)
-        # self.agent_list, self.agent_config = new_builder.load(file_name)
 
         self.agent_list = []
         if file_name is not None:
@@ -71,7 +66,7 @@ class debate:
             select_speaker_message_template="You are managing a debate and your only job is to select the next speaker, each speaker has a name. Follow the debate and decide on who should speak next. The 'Debate_Moderator_Agent' is the first speaker and they will kick off the debate with a topic to debate. Then each of the four debaters will speak and speak only once each. You should start by selecting the 'Affirmative_Constructive_Debater' to provide their opening arguments in the debate.",
             select_speaker_prompt_template="Read the above conversation and your job role, which is managing the debate and choosing the next speaker. The valid speakers can be selected from this list {agentlist}. During the debate the order of debaters are: 1st is the 'Affirmative_Constructive_Debater', 2nd is the 'Negative_Constructive_Debater', 3rd is the 'Affirmative_Rebuttal_Debater', and 4th is the 'Negative_Rebuttal_Debater'. Then 5th will be the 'Debate_Judge' and 6th is the 'Debate_Moderator_Agent'.",
             max_retries_for_selecting_speaker=1,
-            role_for_select_speaker_messages="user",  # CRITICAL TO SET THIS TO USER, IF SYSTEM YOU MAY GET BLANK RESPONSES
+            role_for_select_speaker_messages="user",
             select_speaker_auto_verbose=True,
         )
 
